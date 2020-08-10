76e8c03544f2caca5d70d348317af8b84655dab6
deep_bohara <dpak23bohara@gmail.com>
2020-08-09 22:34:05 -0500

    signin signup cart infodisplay

--- farmFront/src/app/app.component.ts
+++ farmFront/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`
+  <app-header>
+  <router-outlet>
+  <app-footer>`
+  
 })
 export class AppComponent {
   title = 'farmFront';
--- farmFront/src/app/app.module.ts
+++ farmFront/src/app/app.module.ts
@@ -1,12 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SidebarModule} from "ng-sidebar";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup.component';
+import { MainLayoutComponent } from './mainLayout/main-layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './product/products.component';
+import {AddProductComponent} from './farmer/add-product.component';
+import { CheckoutComponent } from './customer/checkout/checkout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { from } from 'rxjs';
+// import { SidebarComponent } from './sidebar/sidebar.component'
+// import {EditproductComponent} from './farmer/add-product.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    MainLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProductComponent,
+    CartComponent,
+    ProductsComponent,
+    CheckoutComponent,
+    NavbarComponent,
+    // SidebarComponent,
+    // EditproductComponent
   ],
   imports: [
     BrowserModule,
