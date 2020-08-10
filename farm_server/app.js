c71318d89783b870bfae9af3358fa43671b8691e
paudelnavin <navinpaudel@outlook.com>
2020-08-10 12:30:31 -0500

    some changes

--- farm_server/app.js
+++ farm_server/app.js
@@ -30,7 +30,10 @@ app.set('view engine', 'jade');
 
 mongoose.connect(process.env.MONGO_URL);
 
+
+
 app.use(bodyParser.json());
+
 app.use(cors());
 
 app.use(logger('dev'));
@@ -54,9 +57,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
