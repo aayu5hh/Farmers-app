e27a3afad88c054cf65c8db0421420384c8a18a6
paudelnavin <navinpaudel@outlook.com>
2020-08-10 20:38:23 -0500

    backend for farmer

--- farm_server/routes/farmers.js
+++ farm_server/routes/farmers.js
@@ -8,118 +8,113 @@ const User = require('../model/user');
 // const router = require('./users.js');
 
 // localhost:3000/farmer/
-router.get('/farmer', (req,res)=>{
-    User.find((err, docs)=>{
-        if(!err){
+router.get('/farmer', (req, res) => {
+    User.find((err, docs) => {
+        if (!err) {
             res.send(docs);
-        }else{
-            console.log('Error in Retriving all Products: '+ JSON.stringify(err, undefined, 2));
+        } else {
+            console.log('Error in Retriving all Products: ' + JSON.stringify(err, undefined, 2));
         }
     });
 });
 
 // localhost:3000/farmer/_farmerid/add
-router.post('farmer/:farmerid/add', (req, res) => {
-    
+router.post('/farmer/:farmerid/add', (req, res) => {
+
     const farmer_id = req.params.farmerid;
-    const product_name = req.body.product_name;
-    const product_description = req.body.product_description;
-    const price = req.body.price;
-    const quantity = req.body.quantity;
-    const product_image = req.body.product_image;
+    const { product_name, product_description, price, quantity, product_image } = req.body.product[0]
+    console.log(req.body)
 
-    try{
-        
-        const ProductData = new User({
-            name:product_name,
-            description:product_description,
-            price:price,
-            quantity:quantity,
-            image:product_image
-        })
-        
-        User.Update({'product._id':product_id}, 
-            {'$set': {'product.$.post': ProductData }},
-             (err, doc) =>{
-                if(!err){
+    try {
+
+        const ProductData = {
+            product_name,
+            product_description,
+            price,
+            quantity,
+            product_image
+        }
+
+        User.update({ '_id': farmer_id },
+            { '$push': { 'product': ProductData } },
+            (err, doc) => {
+                if (!err) {
                     res.send(doc);
-                }else{
-                    console.log('Error in Farmer Product Update: '+ JSON.stringify(err, undefined, 2));
+                } else {
+                    console.log('Error in Farmer Product Update: ' + JSON.stringify(err, undefined, 2));
+                    next(err);
                 }
-             
-        })   
-    }catch(e) {
-        console.log({message: e});
-        res.json({message: e});
+
+            })
+    } catch (e) {
+        console.log({ message: e });
+        res.json({ message: e });
     }
 });
 
-//localhost:3000/farmer/_id 
-router.get('/farmer/:id', (req,res)=>{
-    if(!objectId.isValid(req.params.id)){
-        return res.status(400).send('No records with given id: '+ $(req.params.id));
-    }
+//localhost:3000/farmer/_productid 
+router.get('/farmer/:farmerid/:productid', (req, res) => {
 
-    User.findById(req.params.id, (err, doc) =>{
-        if(!err){
+    User.find({'product._id': req.params.productid}, ['product'], (err, doc) => {
+        if (!err) {
             res.send(doc);
-        }else{
-            console.log('Error in Retriving Products by id: '+ JSON.stringify(err, undefined, 2));
+        } else {
+            console.log('Error in Retriving Products by id: ' + JSON.stringify(err, undefined, 2));
         }
-    });
+    })
 });
 
 // localhost:3000/farmer/_id
-router.put('/farmer/:farmerid/:productid', (req, res)=>{
-    // if(!objectId.isValid(req.params.farmerid && req.params.productid)){
-    //     return res.status(400).send('No records with given farmerid or productid: '+ 
-    //         $(req.params.farmerid)+ " or "+ $(req.params.productid));
-    //     }
+router.patch('/farmer/:farmerid/:productid', (req  ,res) => {
+    console.log('inside the post');
+    const farmer_id = req.params.farmerid
+    const product_id = req.params.productid;
+    const name = 'banana'//req.body.product[0].product_name;
+    const description = req.body.product[0].product_description;
+    const price = req.body.product[0].price;
+    const quantity = req.body.product[0].quantity;
+    const image = req.body.product[0].product_image;
+    console.log(req.body)
+    console.log(name)
 
-    const product_name = req.body.product_name;
-    const product_description = req.body.product_description;
-    const price = req.body.price;
-    const quantity = req.body.quantity;
-    const product_image = req.body.product_image;
+    try {
 
-    try{
-        
-        const ProductData = new User({
-            name:product_name,
-            description:product_description,
-            price:price,
-            quantity:quantity,
-            image:product_image
-        }) 
-        
-        User.findByIdAndUpdate(req.params.farmerid, 
-            {'$set': {'product.$.name': 'Orange' }},
-             {new: true}, (err, doc) =>{
-            if(!err){
-               res.send(doc);
-            }else{
-                console.log('Error in Farmer Product Update: '+ JSON.stringify(err, undefined, 2));
-            }
-        })   
-    }catch(e) {
-        console.log({message: e});
-        res.json({message: e});
+        //     const ProductData = new User({
+        //     name:product_name,
+        //     description:product_description,
+        //     price:price,
+        //     quantity:quantity,
+        //     image:product_image
+        // })
+    
+        User.findOneAndUpdate({'_id':farmer_id, 'product._id':product_id},
+         {$set:{ 'product.$.product_name': name }},
+            (err, doc) => {
+                if (!err) {
+                    res.send(doc);
+                } else {
+                    console.log('Error in Farmer Product Update: ' + JSON.stringify(err, undefined, 2));
+                    
+                }
+
+            })
+    } catch (e) {
+        console.log({ message: e });
+        res.json({ message: e });
     }
+
 });
 
 // localhost:3000/farmer/_id
-router.delete('/:id', (req, res)=>{
-    if(!objectId.isValid(req.params.id)){
-        return res.status(400).send('No records with given id: '+ $(req.params.id));
-    }
+router.delete('/farmer/:farmerid/:productid', (req, res) => {
 
-    User.findByIdAndRemove(req.params.id, (err, doc) =>{
-        if(!err){
+    User.findByIdAndRemove({'product._id': req.params.productid}, ['product'], (err, doc) => {
+        if (!err) {
             res.send(doc);
-        }else{
-            console.log('Error in Farmer Product Delete: '+ JSON.stringify(err, undefined, 2));
+        } else {
+            console.log('Error in Farmer Product Delete: ' + JSON.stringify(err, undefined, 2));
         }
-    })   
+    })
 
 });
 
