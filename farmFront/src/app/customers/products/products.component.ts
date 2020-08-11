ac91ca7372d3ddaf1a30a06b9a7b13246c8c29a8
aayu5hh <me.aayus@gmail.com>
2020-08-11 13:53:10 -0500

    customer products separation of components

--- farmFront/src/app/customers/products/products.component.ts
+++ farmFront/src/app/customers/products/products.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  host: {class: 'row'}
 })
 export class ProductsComponent implements OnInit {
 
