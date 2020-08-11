dfdc4b3719f601ef186769ec0d599eb689ff587f
paudelnavin <navinpaudel@outlook.com>
2020-08-11 09:54:23 -0500

    customer backend

--- farm_server/routes/customer.js
+++ farm_server/routes/customer.js
@@ -6,9 +6,21 @@ var router = express.Router();
 const User = require('../model/user');
 
 //To fetch all users whose role is farmer
-// localhost:3000/farmer/
+// localhost:3000/customer/
 router.get('/customer', (req, res) => {
     User.find({'role':'customer'}, (err, docs) => {
+        if (!err) {
+            res.send(docs);
+        } else {
+            console.log('Error in Retriving all Products: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+//customer to get all products by farmer_id and it will return all products with farmerid
+router.get('/customer/:farmerid', (req, res) => {
+    const farmer_id = req.params.farmerid;
+    User.find({'_id':farmer_id}, ['product'], (err, docs) => {
         if (!err) {
             res.send(docs);
         } else {
