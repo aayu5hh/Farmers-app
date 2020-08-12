376854eb730d7db33374cb648d3c3c20ff5978af
aayu5hh <me.aayus@gmail.com>
2020-08-11 21:52:19 -0500

    Farmer filter working

--- farmFront/src/app/customers/products/products.component.ts
+++ farmFront/src/app/customers/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BackendRequestService } from 'src/app/service/backendRequest.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -13,21 +14,29 @@ export class ProductsComponent implements OnInit {
 
   farmer_id;
   farmerProducts;
+  private subscription: Subscription;
 
   constructor(private backendService: BackendRequestService, private r: Router, private activatedRoute: ActivatedRoute) {
-    activatedRoute.params.subscribe(
-      (param: any) => {this.farmer_id = param['farmer_id']; console.log(this.farmer_id)}
-    )
+    this.subscription = activatedRoute.params.subscribe(
+                          (param: any) => {this.farmer_id = param['farmer_id'];
+                                           console.log(this.farmer_id);
+                                           this.ngOnInit();
+                                          }
+                        )
   }
 
   ngOnInit(): void {
     this.backendService.getFarmerProductsById(this.farmer_id).subscribe(
       (data) => {
-        this.farmerProducts = data[0];
+        this.farmerProducts = data[0].product;
         console.log(this.farmerProducts);
       },
       (err) => console.log(err)
     )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
